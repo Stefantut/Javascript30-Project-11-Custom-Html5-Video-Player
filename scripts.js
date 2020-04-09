0a0e327123078dfe215b6fa7e12808f7f3dcd316
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-04-09 23:33:57 +0100

    Added skip functions and event ls

--- scripts.js
+++ scripts.js
@@ -3,9 +3,34 @@ const player = document.querySelector(".player");
 const video = player.querySelector(".viewer");
 const progress = player.querySelector(".progress");
 const progrssBar = player.querySelector(".progress__filled");
-const toggle = player.querySelector("toggle");
+const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll("player__slider");
-// Build Functions
 
+// Build Functions
+function togglePlay() {
+  const method = video.paused ? "play" : "pause";
+  video[method]();
+  //   if (video.paused) {
+  //     video.play();
+  //   } else {
+  //     video.pause();
+  //   }
+}
+function updateButton() {
+  const icon = this.paused ? "►" : "❚❚";
+  toggle.textContent = icon;
+}
+
+function skip() {
+  // used parseFloat to convert string to number
+  video.currentTime += parseFloat(this.dataset.skip);
+  console.log(video.currentTime);
+}
 // Hook Event listeners
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+toggle.addEventListener("click", togglePlay);
+
+skipButtons.forEach((button) => button.addEventListener("click", skip));
