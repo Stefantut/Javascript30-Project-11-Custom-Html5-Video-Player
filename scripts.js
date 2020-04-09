af8661881824c17d53a45c4778e07752791171e0
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-04-09 22:46:55 +0100

    Got all elements

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Get the elements
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progrssBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector("toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll("player__slider");
+// Build Functions
+
+// Hook Event listeners
